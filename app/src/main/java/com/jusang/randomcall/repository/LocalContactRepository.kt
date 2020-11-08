8e98b00a0e932954c481c92077eb6f1055c0c31d
heyjusang <co9901@wafflestudio.com>
2020-11-08 15:38:00 +0900

    using formatted phone number

--- app/src/main/java/com/jusang/randomcall/repository/LocalContactRepository.kt
+++ app/src/main/java/com/jusang/randomcall/repository/LocalContactRepository.kt
@@ -1,7 +1,6 @@
 package com.jusang.randomcall.repository
 
 import android.Manifest
-import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
 import android.content.pm.PackageManager
@@ -9,10 +8,11 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.provider.ContactsContract
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.telephony.PhoneNumberUtils
 import com.jusang.randomcall.R
 import com.jusang.randomcall.model.ContactModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LocalContactRepository(var context: Context): ContactRepository() {
     override fun getContactList(): List<ContactModel> {
@@ -26,7 +26,8 @@ class LocalContactRepository(var context: Context): ContactRepository() {
         var uri: Uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         var projection: Array<String> = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
             ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER);
+            ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER);
+
 
         var cursor: Cursor? = resolver.query(uri, projection, null, null, null)
 
@@ -35,8 +36,10 @@ class LocalContactRepository(var context: Context): ContactRepository() {
                 var id: String = cursor.getString(0);
                 var name: String = cursor.getString(1);
                 var phone: String = cursor.getString(2);
+                var formattedPhone: String = PhoneNumberUtils.formatNumber(phone, Locale.getDefault().country)
 
-                contacts.add(ContactModel(id, name, phone, R.drawable.profile_sample))
+                // TODO : photo uri
+                contacts.add(ContactModel(id, name, formattedPhone, R.drawable.profile_sample))
             }
 
             cursor.close()
